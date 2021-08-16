485866c6bd5c7a9618bb1890774c03868b3ea78c
Dora-H <70878758+Dora-H@users.noreply.github.com>
2021-08-16 16:16:09 +0800

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,49 @@
+# for x in 'ABC':
+#     for y in'123':
+#         print(x+y)
+
+# count=0
+# for x in range(5):
+#     for y in range(10):
+#         count+=1
+# print(count)
+# print(x,y)
+
+# s=int(input('enter a number: '))
+# if 1<=s<=5:
+#     j=0
+#     while j<5:
+#         for n in range(1,6):
+#             print(n,end=' ')
+#         j+=1
+#         print()
+# else:
+#     print('invalid number')
+
+# h=int(input('enter a number: '))
+# j=0
+# while j<h:
+#     for k in range(1,h+1):
+#         print(k, end=' ')
+#     j+=1
+#     print()
+
+# h=int(input('enter a number: '))
+# for _ in range(h):
+#     for k in range(1,h+1):
+#         print(k, end=' ')
+#     print()
+
+# w=int(input('enter a number: '))
+# for y in range(1,w+1):
+#     for k in range(y,w+y):
+#         print(k, end=' ')
+#     print()
+# n=int(input('enter a number: '))
+# b='#'*1+'#'*(n-2)+'#'*1
+# c='#'*1+' '*(n-2)+'#'*1
+# b1='#'*1+'#'*(n-2)+'#'*1
+# print(b)
+# print(c)
+# print(c)
+# print(b1)
